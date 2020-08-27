664c0c9f3415bfbbea882a3a93bee59f64117e52
guihRovetta <guilherme020213@hotmail.com>
2020-08-26 22:28:39 -0300

    ♻️ refactored classes & connections controllers

--- src/app/controllers/ClassesController.ts
+++ src/app/controllers/ClassesController.ts
@@ -1,6 +1,8 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import db from '../../database/connection';
+
+import { ErrorHandler } from '../helpers/error';
 
 import ClassesRepository from '../repositories/ClassesRepository';
 import UsersRepository from '../repositories/UsersRepository';
@@ -18,193 +20,205 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
-  async index(request: Request, response: Response) {
-    const filters = request.query;
-
-    const subject = filters.subject as string;
-    const week_day = Number(filters.week_day);
-    const time = filters.time as string;
-
-    const limit = Number(filters.limit);
-    const page = Number(filters.page);
-
-    if (!subject || !week_day || !time) {
-      return response.status(400).json({
-        error: 'Missing filters to search classes',
-      });
-    }
-
-    const timeInMinutes = convertHourToMinutes(time);
-
-    const classes = await classesRepository.findAll(
-      week_day,
-      timeInMinutes,
-      subject,
-      limit,
-      page
-    );
-
-    return response.json(classes);
-  }
-
-  async create(request: Request, response: Response) {
-    const { avatar, whatsapp, bio, subject, cost, schedule } = request.body;
-
-    const id = Number(request.userId);
-
-    if (!avatar || !whatsapp || !bio) {
-      return response.status(400).json({
-        error: 'Missing personal data to create class',
-      });
-    }
-
-    if (!subject || !cost || !schedule) {
-      return response.status(400).json({
-        error: 'Missing class data to create class',
-      });
-    }
-
-    const user = await usersRepository.findById(id);
-
-    if (!user) {
-      return response.status(400).json({ message: 'User not found.' });
-    }
-
-    const classExists = await classesRepository.findById(id);
-
-    if (classExists) {
-      return response.status(400).json({
-        message: 'This user already has a class registered, try to update it.',
-      });
-    }
-
-    let trx = await db.transaction();
-
+  async index(request: Request, response: Response, next: NextFunction) {
     try {
-      trx = await usersRepository.update(trx, id, avatar, whatsapp, bio);
+      const filters = request.query;
 
-      const user_id = user.id;
+      const subject = filters.subject as string;
+      const week_day = Number(filters.week_day);
+      const time = filters.time as string;
 
-      const created = await classesRepository.create(
-        trx,
+      const limit = Number(filters.limit);
+      const page = Number(filters.page);
+
+      if (!subject || !week_day || !time) {
+        throw new ErrorHandler(400, 'Missing filters to search classes');
+      }
+
+      const timeInMinutes = convertHourToMinutes(time);
+
+      const classes = await classesRepository.findAll(
+        week_day,
+        timeInMinutes,
         subject,
-        cost,
-        user_id
+        limit,
+        page
       );
 
-      const insertedClassesIds = created.insertedClassesIds;
-      trx = created.trx;
-
-      const class_id = insertedClassesIds[0];
-
-      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-          class_id,
-          week_day: scheduleItem.week_day,
-          from: convertHourToMinutes(scheduleItem.from),
-          to: convertHourToMinutes(scheduleItem.to),
-        };
-      });
-
-      trx = await classScheduleRepository.create(trx, classSchedule);
-
-      await trx.commit();
-
-      return response.status(201).send();
-    } catch (err) {
-      await trx.rollback();
-
-      return response.status(400).json({
-        error: 'Unexpected error while creating new class',
-      });
+      return response.json(classes);
+    } catch (error) {
+      next(error);
     }
   }
 
-  async update(request: Request, response: Response) {
-    const {
-      name,
-      lastname,
-      email,
-      avatar,
-      whatsapp,
-      bio,
-      subject,
-      cost,
-      schedule,
-    } = request.body;
-
-    if (
-      !name ||
-      !lastname ||
-      !email ||
-      !avatar ||
-      !whatsapp ||
-      !bio ||
-      !subject ||
-      !cost ||
-      !schedule
-    ) {
-      return response.status(400).json({
-        error: 'Missing fields to update user info',
-      });
-    }
-
-    const id = Number(request.userId);
-
-    const user = await usersRepository.findById(id);
-    const user_id = user.id;
-
-    const userClass = await classesRepository.findById(user_id);
-    const class_id = userClass.id;
-
-    let trx = await db.transaction();
-
+  async create(request: Request, response: Response, next: NextFunction) {
     try {
-      trx = await usersRepository.fullUpdate(
-        trx,
-        id,
+      const { avatar, whatsapp, bio, subject, cost, schedule } = request.body;
+
+      const id = Number(request.userId);
+
+      if (!avatar || !whatsapp || !bio) {
+        throw new ErrorHandler(400, 'Missing personal data to create class');
+      }
+
+      if (!subject || !cost || !schedule) {
+        throw new ErrorHandler(400, 'Missing class data to create class');
+      }
+
+      const user = await usersRepository.findById(id);
+
+      if (!user) {
+        throw new ErrorHandler(400, 'User not found');
+      }
+
+      const classExists = await classesRepository.findById(id);
+
+      if (classExists) {
+        throw new ErrorHandler(
+          400,
+          'This user already has a class registered, try to update it'
+        );
+      }
+
+      let trx = await db.transaction();
+
+      try {
+        trx = await usersRepository.update(trx, id, avatar, whatsapp, bio);
+
+        const user_id = user.id;
+
+        const created = await classesRepository.create(
+          trx,
+          subject,
+          cost,
+          user_id
+        );
+
+        const insertedClassesIds = created.insertedClassesIds;
+        trx = created.trx;
+
+        const class_id = insertedClassesIds[0];
+
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+          return {
+            class_id,
+            week_day: scheduleItem.week_day,
+            from: convertHourToMinutes(scheduleItem.from),
+            to: convertHourToMinutes(scheduleItem.to),
+          };
+        });
+
+        trx = await classScheduleRepository.create(trx, classSchedule);
+
+        await trx.commit();
+
+        return response.status(201).send();
+      } catch (err) {
+        await trx.rollback();
+
+        throw new ErrorHandler(
+          400,
+          'Unexpected error while creating new class'
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const {
         name,
         lastname,
         email,
         avatar,
         whatsapp,
-        bio
-      );
+        bio,
+        subject,
+        cost,
+        schedule,
+      } = request.body;
 
-      trx = await classesRepository.update(trx, user_id, subject, cost);
+      if (
+        !name ||
+        !lastname ||
+        !email ||
+        !avatar ||
+        !whatsapp ||
+        !bio ||
+        !subject ||
+        !cost ||
+        !schedule
+      ) {
+        throw new ErrorHandler(400, 'Missing fields to update user info');
+      }
 
-      trx = await classScheduleRepository.delete(trx, class_id);
+      const id = Number(request.userId);
 
-      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-          class_id,
-          week_day: scheduleItem.week_day,
-          from: convertHourToMinutes(scheduleItem.from),
-          to: convertHourToMinutes(scheduleItem.to),
-        };
-      });
+      const user = await usersRepository.findById(id);
+      const user_id = user.id;
 
-      trx = await classScheduleRepository.create(trx, classSchedule);
+      const userClass = await classesRepository.findById(user_id);
+      const class_id = userClass.id;
 
-      await trx.commit();
+      let trx = await db.transaction();
 
-      return response.status(201).send();
-    } catch (err) {
-      await trx.rollback();
+      try {
+        trx = await usersRepository.fullUpdate(
+          trx,
+          id,
+          name,
+          lastname,
+          email,
+          avatar,
+          whatsapp,
+          bio
+        );
 
-      return response.status(400).json({
-        error: 'Unexpected error while updating a class',
-      });
+        trx = await classesRepository.update(trx, user_id, subject, cost);
+
+        trx = await classScheduleRepository.delete(trx, class_id);
+
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+          return {
+            class_id,
+            week_day: scheduleItem.week_day,
+            from: convertHourToMinutes(scheduleItem.from),
+            to: convertHourToMinutes(scheduleItem.to),
+          };
+        });
+
+        trx = await classScheduleRepository.create(trx, classSchedule);
+
+        await trx.commit();
+
+        return response.status(201).send();
+      } catch (err) {
+        await trx.rollback();
+
+        throw new ErrorHandler(400, 'Unexpected error while updating a class');
+      }
+    } catch (error) {
+      next(error);
     }
   }
 
-  async get(request: Request, response: Response) {
-    const { id } = request.params;
+  async get(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { id } = request.params;
 
-    const user_id = Number(id);
+      const user_id = Number(id);
 
-    const data = await classesRepository.findClassSchedule(user_id);
+      const data = await classesRepository.findClassSchedule(user_id);
 
-    return response.json(data);
+      return response.json(data);
+    } catch {
+      const error = new ErrorHandler(
+        400,
+        'Unexpected error while listing your class schedule'
+      );
+      next(error);
+    }
   }
 }
--- src/app/controllers/ConnectionsController.ts
+++ src/app/controllers/ConnectionsController.ts
@@ -1,29 +1,38 @@
-import { Request, Response } from 'express';
-import db from '../../database/connection';
+import { Request, Response, NextFunction } from 'express';
+
+import { ErrorHandler } from '../helpers/error';
+
 import ConnectionsRepository from '../repositories/ConnectionsRepository';
 
 const connectionsRepository = new ConnectionsRepository();
 
 export default class ConnectionsController {
-  async index(request: Request, response: Response) {
-    const totalConnections = await connectionsRepository.count();
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const totalConnections = await connectionsRepository.count();
 
-    const { total } = totalConnections[0];
+      const { total } = totalConnections[0];
 
-    return response.json({ total });
+      return response.json({ total });
+    } catch {
+      const error = new ErrorHandler(
+        400,
+        'Unexpected error while counting number of connections'
+      );
+      next(error);
+    }
   }
 
-  async create(request: Request, response: Response) {
-    const { user_id } = request.body;
-
+  async create(request: Request, response: Response, next: NextFunction) {
     try {
+      const { user_id } = request.body;
+
       await connectionsRepository.create(user_id);
 
       return response.status(201).json();
     } catch {
-      return response.status(400).json({
-        error: 'Unable to create connection',
-      });
+      const error = new ErrorHandler(400, 'Unable to create this connection');
+      next(error);
     }
   }
 }
